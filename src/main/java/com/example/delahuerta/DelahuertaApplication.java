00e16b28416d3accd456661788d7d91a00bc3013
Lafrul <laurafranco2003@gmail.com>
2025-11-05 17:22:51 -0500

    first commit

---
+++ src/main/java/com/example/delahuerta/DelahuertaApplication.java
@@ -0,0 +1,13 @@
+package com.example.delahuerta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelahuertaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelahuertaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/delahuerta/DelahuertaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.delahuerta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelahuertaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
